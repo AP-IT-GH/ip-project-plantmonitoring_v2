a8907ce6e5f7c6fe07547143b0fe7d92e9543c36
Unknown <cristian.baba09@yahoo.com>
2019-03-20 18:27:28 +0100

    update php connection + data

--- internationalproject/src/app/app-routing.module.ts
+++ internationalproject/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: '', redirectTo:'login', pathMatch: 'full'},
   {path : 'home' , component: HomeComponent},
   {path : 'sensors', component: SensorsComponent},
   {path : 'history', component: HistoryComponent},
