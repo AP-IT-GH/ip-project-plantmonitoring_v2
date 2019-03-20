7aa1e9517b05daa54f1f8373977242521a5ac735
Unknown <cristian.baba09@yahoo.com>
2019-03-20 09:47:13 +0100

    php connection + data

---
+++ PHPApi/getlist.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Returns the list of sensors.
+ */
+require 'connect.php';
+    
+$sensors = [];
+$sql = "SELECT sensor_id,temperature , humidity , pressure , altitude , ativo FROM sensors";
+
+if($result = mysqli_query($con,$sql))
+{
+  $cr = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $sensors[$cr]['sensor_id']    = $row['sensor_id'];
+    $sensors[$cr]['temperature'] = $row['temperature'];
+    $sensors[$cr]['humidity'] = $row['humidity'];
+    $sensors[$cr]['pressure'] = $row['pressure'];
+    $sensors[$cr]['altitude'] = $row['altitude'];
+    $sensors[$cr]['ativo'] = $row['ativo'];
+    $cr++;
+  }
+    
+  echo json_encode(['data'=>$sensors]);
+}
+else
+{
+  http_response_code(404);
+}
